e3c2dc85d9adfc6d19d2e8a3c953ba7bb2f6ac68
sunimalherath <sunimaldv@gmail.com>
2020-10-09 23:22:03 +1100

    Initial Commit

---
+++ SmackUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SmackUI
+//
+//  Created by Sunimal Herath on 9/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
